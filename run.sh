daf15177881556900c807e6c7df087b46f6173dd
damithc <1673303+damithc@users.noreply.github.com>
2025-11-15 17:02:24 +0800

    Stop cron job

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --master
-java -jar RepoSense.jar --config ./config --since 08/08/2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./config --until 28/11/2025 --since 08/08/2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
